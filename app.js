207547d32d08c7bd39d7b8c00ded7e6abf3641a5
marts-dev <8363978+marts-dev@users.noreply.github.com>
2020-06-01 15:01:42 +0800

    Challenge 18

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -43,10 +44,11 @@ app.get("/compose", function (req, rsp) {
 });
 
 app.get("/posts/:topic", function (req, rsp) {
-    const queryTopic = req.params.topic;
+    const queryTopic = _.kebabCase(req.params.topic);
     blogPost.forEach((post) => {
-        const storedTopic = post.title;
+        const storedTopic = _.kebabCase(post.title);
         if (storedTopic === queryTopic) {
+            console.log(queryTopic);
             console.log("Found");
         } else {
             console.log("Not found");
